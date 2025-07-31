e86c53de504cbf34d66cad4464df5fe1db78ccef
Dragoş <edragos33@gmail.com>
2025-07-31 15:17:34 +0200

    small details added

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ window_y = 480
 
 # Colors
 black = pygame.Color(0, 0, 0)
-white = pygame.color(255,255,255)
-red = pygame.color(255,0,0)
+white = pygame.Color(255,255,255)
+red = pygame.Color(255,0,0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
+
+
+pygame.init()
+
+pygame.display.set_caption('Snake')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+fps = pygame.time.Clock()
