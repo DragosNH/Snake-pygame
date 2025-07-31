ec4d80104ad9085913e71334c2bc894a1940a470
Dragoş <edragos33@gmail.com>
2025-07-31 15:13:52 +0200

    colors and imports made

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# Snake-pygame" 
+# Snake game in pygame
+
+this is a project that I am doing following the tutorial of "geeks for geeks". You can find the tutorial by [clicking here](https://www.geeksforgeeks.org/python/snake-game-in-python-using-pygame-module/).
---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import time
+import random
+
+snake_speed = 15
+
+# Widow size
+window_x = 720
+window_y = 480
+
+# Colors
+black = pygame.Color(0, 0, 0)
+white = pygame.color(255,255,255)
+red = pygame.color(255,0,0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
